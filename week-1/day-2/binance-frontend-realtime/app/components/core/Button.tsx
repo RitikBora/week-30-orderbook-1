e3d53f428d259a5165f4bee63c00ac8a1881b39f
RitikBora <ritik.bora@xecurify.com>
2024-10-28 15:41:50 +0530

    added animations

--- week-1/day-2/binance-frontend-realtime/app/components/core/Button.tsx
+++ week-1/day-2/binance-frontend-realtime/app/components/core/Button.tsx
@@ -10,7 +10,7 @@ export const PrimaryButton = ({ children, onClick }: { children: string, onClick
 } 
 
 export const SuccessButton = ({ children, onClick }: { children: string, onClick?: () => void }) => {
-    return <button type="button" className="text-center font-semibold rounded-lg focus:ring-green-200 focus:none focus:outline-none hover:opacity-90 disabled:opacity-80 disabled:hover:opacity-80 relative overflow-hidden h-[60px] w-[150px] px-3 py-1.5 mr-4 "
+    return <button type="button" className="text-center font-semibold rounded-lg focus:ring-green-200 focus:none focus:outline-none hover:opacity-90 disabled:opacity-80 disabled:hover:opacity-80 relative overflow-hidden h-[60px] w-[250px] px-3 py-1.5 mr-4 "
     onClick={onClick} style={{fontSize : '1.3rem'}}>
         <div className="absolute inset-0 bg-green-500 opacity-[16%]"></div>
         <div className="flex flex-row items-center justify-center gap-4"><p className="text-green-500">{children}</p></div>
--- week-1/day-2/binance-frontend-realtime/app/page.tsx
+++ week-1/day-2/binance-frontend-realtime/app/page.tsx
@@ -1,47 +1,84 @@
-"use client"
+"use client";
 import Image from "next/image";
 import { PrimaryButton, SuccessButton } from "./components/core/Button";
 import { useRouter } from "next/navigation";
+import { motion } from "framer-motion";
+
+const parentVariants = {
+  animate: {
+    transition: {
+      staggerChildren: 0.3, 
+    },
+  },
+};
+
+const childVariants = {
+  initial: { opacity: 0, y: 20 },
+  animate: { opacity: 1, y: 0, transition: { duration: 0.6 } },
+};
+
+const childVariantsDown = {
+  initial: { opacity: 0, y: 0 },
+  animate: { opacity: 1, y: 20, transition: { duration: 0.6 } },
+}
 
 export default function Home() {
   const router = useRouter();
+  
   return (
-    <div className=" h-auto grid grid-cols-3">
-        <div className="col-span-2 m-16" >
+    <motion.div
+      className="h-auto grid grid-cols-3"
+      variants={parentVariants}
+      initial="initial"
+      animate="animate"
+    >
+      <div className="col-span-2 m-16">
+        <motion.div
+          className="text-white font-bold"
+          style={{ fontSize: "5rem" }}
+          variants={childVariants}
+        >
+          Look First/
+        </motion.div>
+        
+        <motion.div
+          className="text-white font-bold"
+          style={{ fontSize: "5rem" }}
+          variants={childVariants}
+        >
+          Then Leap.
+        </motion.div>
+        
+        <motion.div
+          className="text-white font-bold"
+          style={{ fontSize: "1.5rem" }}
+          variants={childVariants}
+        >
+          <p className="pt-5 text-slate-500 text-lg leading-snug">The best trades require research, then <br />commitment.</p>
+        </motion.div>
+        
+       
+          <motion.div className="py-10 mr-2 flex" variants={childVariantsDown}>
+            <SuccessButton onClick={() => router.push("/markets")}>
+              Explore Markets
+            </SuccessButton>
+            <PrimaryButton onClick={() => router.push("/trade/SOL_USDC")}>
+              Solana
+            </PrimaryButton>
+          </motion.div>
           
-              <div className="text-white font-bold" style={{fontSize: '5rem'}}>
-                 Look First/
-              </div>
-              <div className="text-white font-bold" style={{fontSize: '5rem'}}>
-                 Then Leap.
-              </div>
-              <div className="text-white font-bold" style={{fontSize: '1.5rem'}}>
-                 <p>The best trades require research, then <br />commitment.</p>
-              </div>
 
-              <div className="flex mt-10">
-                <div className="p-2 mr-2">
-                    <SuccessButton onClick={() => router.push("/markets")}>All Markets</SuccessButton>
-                    <PrimaryButton onClick={() => {
-                
-                      router.push('/trade/SOL_USDC');
-                    }}>Solana</PrimaryButton>
-                </div>
-            </div>
-        </div>
-       <div className="col-span-1 relative">
-    <div className="absolute bottom-0 right-24 text-white" >
-        <div style={{ fontSize: "1.5rem" }}>
-          MarketNexus.inc
-        </div>
-        <div className="text-gray-400 flex justify-end" 
-        style={{fontSize: "1rem"}}>
+        
+      </div>
+      
+      <div className="col-span-1 relative">
+        <div className="absolute bottom-0 right-24 text-white">
+          <div style={{ fontSize: "1.5rem" }}>XChange.inc</div>
+          <div className="text-gray-400 flex justify-end" style={{ fontSize: "1rem" }}>
             CEO: Ritik Bora
+          </div>
         </div>
-    </div>
-    
-</div>
-
-    </div>
+      </div>
+    </motion.div>
   );
 }
